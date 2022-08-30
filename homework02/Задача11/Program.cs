c51f2491b62f0d920412ed24daf7c951c38f500f
Nikolay <nik1998293@gmail.com>
2022-08-30 21:58:24 +0300

    Задача на семинаре решена

---
+++ homework02/Задача11/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка 
+// [10, 99] и показывает наибольшую цифру числа
+
+Void Ex01()
+{
+int val = new Random().Next(10, 100);
+int a = val / 10;
+int b = val % 10;
+Console.Writeline(val);
+
+if (a > b)
+{
+    Console.Writeline(a);
+}
+else 
+{
+    Console.Writeline(b);
+}
+}
+Ex01();
